d98cba1715a87f107e025a5db1b01b56fc358649
MarekSosnicki <marek.p.sosnicki@gmail.com>
2024-11-09 18:49:13 +0100

    Problem generator improvements

--- examples/problem_generator.rs
+++ examples/problem_generator.rs
@@ -1,9 +1,13 @@
 use chrono::{Duration, TimeZone, Utc};
 use clap::Parser;
+use itertools::{iproduct, Itertools};
+use rand::prelude::SliceRandom;
 use rand::{thread_rng, Rng};
 use std::path::PathBuf;
 
-use table_problem::problem::{Person, ProblemDescription, TableDay, MAX_PEOPLE_FOR_TABLE, PersonVisit};
+use table_problem::problem::{
+    Person, PersonVisit, ProblemDescription, TableDay, MAX_PEOPLE_FOR_TABLE,
+};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -29,33 +33,42 @@ fn main() {
     for problem_id in 0..args.no_of_problems {
         let start_date = Utc.with_ymd_and_hms(2024, 3, 3, 0, 0, 0).unwrap();
         let no_of_people = thread_rng().gen_range(args.min_people..args.max_people);
-        let min_no_of_table_days = no_of_people / MAX_PEOPLE_FOR_TABLE + 1;
-        // no required to allocate all people + 5%
-        let no_of_table_days = min_no_of_table_days + min_no_of_table_days * 105 / 100;
+        let min_no_of_table_days = no_of_people / MAX_PEOPLE_FOR_TABLE;
+        // no required to allocate all people + 5% min 2 day tables added
+        let no_of_table_days = min_no_of_table_days + (min_no_of_table_days * 105 / 100).max(2);
         // no of tables
-        let no_of_tables = (no_of_table_days + 6) / 7;
+        let no_of_tables = (no_of_people / MAX_PEOPLE_FOR_TABLE / 2).max(3);
+
+        let days = (0..7).map(|day| start_date + Duration::days((day + 1) as i64));
+        let mut all_possible_day_tables = iproduct!(days,  0..no_of_tables)
+            .collect_vec();
+        all_possible_day_tables.shuffle(&mut thread_rng());
+
+        println!(
+            "Generating problem {} no_of_people {} no_of_table_days {} no_of_tables {}",
+            problem_id, no_of_people, no_of_table_days, no_of_tables
+        );
 
         let problem_data = ProblemDescription {
-            tables: (0..7)
-                .flat_map(|day| {
-                    (0..no_of_tables).map(move |table_id| TableDay {
-                        id: day + 7 * table_id,
-                        table_id,
-                        date: start_date + Duration::days((day + 1) as i64),
-                    })
+            tables: all_possible_day_tables
+                .into_iter()
+                .enumerate()
+                .map(|(id, (date, table_id))| TableDay {
+                    id,
+                    table_id,
+                    date,
                 })
+                .take(no_of_table_days)
                 .collect(),
             people: (0..no_of_people)
-                .map(|id| {
-                    Person {
-                        id,
-                        visits: (0..thread_rng().gen_range(0..3))
-                            .map(|_| PersonVisit {
-                                table_id: thread_rng().gen_range(1..no_of_tables),
-                                at: start_date - Duration::days(thread_rng().gen_range(1..30)),
-                            })
-                            .collect()
-                    }
+                .map(|id| Person {
+                    id,
+                    visits: (0..thread_rng().gen_range(0..3))
+                        .map(|_| PersonVisit {
+                            table_id: thread_rng().gen_range(0..no_of_tables),
+                            at: start_date - Duration::days(thread_rng().gen_range(1..30)),
+                        })
+                        .collect(),
                 })
                 .collect(),
             people_relations: (0..no_of_people)
